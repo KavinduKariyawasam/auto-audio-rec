5654c76ce4c69cd945a6a224adc8ede307107749
Kavindu Kariyawasam <72915403+KavinduKariyawasam@users.noreply.github.com>
2024-04-25 15:34:51 +0530

    Updated

--- src/auto_audio_rec/frontend/main.js
+++ src/auto_audio_rec/frontend/main.js
@@ -1,11 +1,16 @@
-// The `Streamlit` object exists because our html file includes
-// `streamlit-component-lib.js`.
-// If you get an error about "Streamlit" not being defined, that
-// means you're missing that file.
-
-function sendValue(value) {
-  Streamlit.setComponentValue(value);
+function onKeyUp(event) {
+  Streamlit.setComponentValue(event.target.value);
 }
+
+const debounce = (callback, wait) => {
+  let timeoutId = null;
+  return (...args) => {
+    window.clearTimeout(timeoutId);
+    timeoutId = window.setTimeout(() => {
+      callback.apply(null, args);
+    }, wait);
+  };
+};
 
 /**
  * The component's render function. This will be called immediately after
@@ -13,31 +18,88 @@ function sendValue(value) {
  * component gets new data from Python.
  */
 function onRender(event) {
-  // Only run the render code the first time the component is loaded.
+  // Get the RenderData from the event
+
+  // This is called on every render to allow changing themes via settings
+  const root = document.getElementById("root");
+
+  root.style.setProperty("--base", event.detail.theme.base);
+  root.style.setProperty("--primary-color", event.detail.theme.primaryColor);
+  root.style.setProperty(
+    "--background-color",
+    event.detail.theme.backgroundColor
+  );
+  root.style.setProperty(
+    "--secondary-background-color",
+    event.detail.theme.secondaryBackgroundColor
+  );
+  root.style.setProperty("--text-color", event.detail.theme.textColor);
+  root.style.setProperty("--font", event.detail.theme.font);
+
   if (!window.rendered) {
-    // Grab the label and default value that the user specified
-    const { label, value } = event.detail.args;
+    const {
+      label,
+      value,
+      debounce: debounce_time,
+      max_chars,
+      type,
+      placeholder,
+      disabled,
+      label_visibility,
+    } = event.detail.args;
 
-    // Set the label text to be what the user specified
-    const label_el = document.getElementById("label");
-    label_el.innerText = label;
-
-    // Set the default value to be what the user specified
     const input = document.getElementById("input_box");
-    if (value) {
+    const label_el = document.getElementById("label");
+
+    if (label_el) {
+      label_el.innerText = label;
+    }
+
+    if (value && !input.value) {
       input.value = value;
     }
 
-    // On the keyup event, send the new value to Python
-    input.onkeyup = (event) => sendValue(event.target.value);
+    if (type == "password") {
+      input.type = "password";
+    } else {
+      input.type = "text";
+    }
+
+    if (max_chars) {
+      input.maxLength = max_chars;
+    }
+
+    if (placeholder) {
+      input.placeholder = placeholder;
+    }
+
+    if (disabled) {
+      input.disabled = true;
+      label.disabled = true;
+      // Add "disabled" class to root element
+      root.classList.add("disabled");
+    }
+
+    if (label_visibility == "hidden") {
+      root.classList.add("label-hidden");
+    } else if (label_visibility == "collapsed") {
+      root.classList.add("label-collapsed");
+      Streamlit.setFrameHeight(45);
+    }
+
+    if (debounce_time > 0) {
+      // is false if debounce_time is 0 or undefined
+      input.onkeyup = debounce(onKeyUp, debounce_time);
+    } else {
+      input.onkeyup = onKeyUp;
+    }
+
+    // Render with the correct height
+    Streamlit.setFrameHeight(73);
 
     window.rendered = true;
   }
 }
 
-// Render the component whenever python send a "render event"
 Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender);
-// Tell Streamlit that the component is ready to receive events
 Streamlit.setComponentReady();
-// Render with the correct height, if this is a fixed-height component
-Streamlit.setFrameHeight(85);
